05c8886cda0e6a69459b06192fe6b55890c2e198
Fareen Ansari <fareen.ansari.in@freecharge.com>
2024-10-09 16:50:41 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from './Button';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
@@ -8,7 +9,9 @@ function Footer() {
         <h2 className="font-bold text-3xl  leading-snug">
             Ready to get your Creativity back?<br/>Start to Explore today.
         </h2>
-        <Button href="https://ansarifareen.github.io/">Get Started</Button>
+        <Link to="/">
+        <Button href="#">Get Started</Button>
+        </Link>
         <div className="mt-3 xl:mt-5">
             <nav className="flex flex-wrap justify-center text-lg font-medium">
                 <div className="px-5 py-2"><a href="#">Contact</a></div>
